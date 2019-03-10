3083082a2a40f663818b72735a6d44a911b590a1
nhinguyen120 <47712578+nhinguyen120@users.noreply.github.com>
2019-03-10 22:02:23 +0700

    fix layout

--- app/src/main/java/com/example/nhi/dating/Login_Activity.java
+++ app/src/main/java/com/example/nhi/dating/Login_Activity.java
@@ -3,11 +3,11 @@ package com.example.nhi.dating;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignUp_Activity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_);
+        setContentView(R.layout.activity_login_);
     }
 }
