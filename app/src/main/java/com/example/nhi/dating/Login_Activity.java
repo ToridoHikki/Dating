7dde99cfe4ec45037a62af88c83e7c083b6a2437
Huynh Le Kim Ngan <nhanhuynh.kim23@gmail.com>
2019-03-21 10:22:06 +0700

    set event
    set event 2 layout register and login
    
    Co-Authored-By: Nhi Nguyen <nhinguyen120@users.noreply.github.com>

--- app/src/main/java/com/example/nhi/dating/Login_Activity.java
+++ app/src/main/java/com/example/nhi/dating/Login_Activity.java
@@ -2,12 +2,43 @@ package com.example.nhi.dating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
-
+    private EditText et_email_login,et_password_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+
+        et_email_login = findViewById(R.id.et_email_login);
+        et_password_login = findViewById(R.id.et_password_login);
+
+        et_email_login.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EditText text = (EditText) getCurrentFocus();
+                if ( text.length()==8 ||text.length()==28){
+                    View next = text.focusSearch(View.FOCUS_DOWN);
+                    if(next!=null) {
+                        next.requestFocus();
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nhi/dating/Register_Activity.java
+++ app/src/main/java/com/example/nhi/dating/Register_Activity.java
@@ -1,13 +1,70 @@
 package com.example.nhi.dating;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register_Activity extends AppCompatActivity {
-
+    private EditText et_email_register, et_password_register, et_password2_register;
+    private CardView cv_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
+
+        et_email_register=findViewById(R.id.et_email_register);
+        et_password_register=findViewById(R.id.et_password_register);
+        et_password2_register=findViewById(R.id.et_password2_register);
+        cv_register=findViewById(R.id.cv_register);
+
+        et_email_register.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EditText text = (EditText) getCurrentFocus();
+                if ( text.length()==8 ||text.length()==28){
+                    View next = text.focusSearch(View.FOCUS_DOWN);
+                    if(next!=null) {
+                        next.requestFocus();
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        cv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pass, pass2;
+                pass=et_password_register.getText().toString().trim();
+                pass2=et_password2_register.getText().toString().trim();
+                if(pass.equals(pass2)){
+                    Toast.makeText(Register_Activity.this,"Sign Up Success",Toast.LENGTH_LONG).show();
+                    Intent login_intent = new Intent(Register_Activity.this,EditProfile_Activity.class);
+                    startActivity(login_intent);
+                }
+                else{
+                    Toast.makeText(Register_Activity.this,"Sign Up Fail, Please retype password",Toast.LENGTH_LONG).show();
+                    et_password_register.setText("");
+                    et_password2_register.setText("");
+                }
+
+
+            }
+        });
+
     }
 }
